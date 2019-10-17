2d9bc04b72b7aee9b27e3975298ae3521210c080
sidbhushan <sidhbhushan@gmail.com>
2019-10-16 21:55:54 -0600

    Added momentum and distance from a,t,v_o equation

--- equations.js
+++ equations.js
@@ -9,6 +9,14 @@ const PHYSICS_EQUATIONS = [
     },
     {
         result: 'd',
+        inputs: ['a', 't', 'v_o'],
+        equation: (a, t, v_o) => 0.5 * a * Math.pow(t, 2) + v_o * t,
+        latex: "d=\\frac{1}{2}at^2+v_ot",
+        substitute: (a, t, v_o) => `d=\\frac{1}{2}(${a}\\text{ms}^{-2})(${t}\\text{s})^2+(${v_o}\\text{ms}^{-1})(${t}\\text{s})`,
+        resultFormat: d => `d=${d}\\text{m}`
+    },
+    {
+        result: 'd',
         inputs: ['v_{ave}', 't'],
         equation: (v_ave, t) => v_ave * t,
         latex: "d=v_{ave}t",
@@ -30,5 +38,20 @@ const PHYSICS_EQUATIONS = [
         latex: "E_k=\\frac{1}{2}mv^2",
         substitute: (m, v) => `E_k=\\frac{1}{2}(${m}\\text{kg})(${v}\\text{ms}^{-1})^2`,
         resultFormat: E_k => `E_k=${E_k}\\text{J}`
+    },
+    {
+        result: 'p',
+        inputs: ['m', 'v'],
+        equation: (m, v) => m * v,
+        latex: "p=mv",
+        substitute: (m, v) => `p=${m}\\text{kg}\\cdot${v}\\text{ms}^{-1}`,
+        resultFormat: p => `p=${p}\\text{Ns}`
     }
 ]
+
+const UNITS = {
+    m: '\\text{kg}',
+    v: '\\text{ms}^-1',
+    a: '\\text{ms}^-2',
+    d: '\\text{m}'
+}
